d161b1eac53e85e0f05b22ac4fbd13591132c7f1
Chetan <chetanjogi1@gmail.com>
2018-08-15 14:48:29 +0530

    Initial Commit

---
+++ storev2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(storev2.Startup))]
+namespace storev2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
