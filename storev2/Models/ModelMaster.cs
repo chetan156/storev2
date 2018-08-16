cebea79cb1b4188b31b9af30d143c705b138ed80
SwatiHedaoo <hswati888@gmail.com>
2018-08-16 22:13:13 +0530

    Check in Model Master

---
+++ storev2/Models/ModelMaster.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace storev2.Models
+{
+    [Table("ModelMaster")]
+    public class ModelMaster
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ModelId { get; set; }
+        [Required(ErrorMessage ="Please Enter Model Name")]
+        public string ModelName { get; set; }
+        [ForeignKey("CompanyMaster")]
+        [Required(ErrorMessage ="Please Select Company Name")]
+        public int CompanyId { get; set; }
+        [ForeignKey("CategoryMaster")]
+        [Required(ErrorMessage = "Please Select Category Name")]
+        public int CategoryId { get; set; }
+
+        public virtual CompanyMaster CompanyMaster { get; set; }
+        public virtual CategoryMaster CategoryMaster { get; set; }
+    }
+    public enum Model
+    {
+        Index,
+        ModelId,
+        ModelName,
+        CompanyId,
+        CategoryId
+    }
+}
